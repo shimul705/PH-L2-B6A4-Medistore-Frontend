84616f2b301b53566ed785a6d206f026faece77e
Shimul705 <freelancershimul172@gmail.com>
2026-02-02 16:34:02 +0600

    fixed frontend url

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,6 +2,6 @@ export const env = {
   apiBaseUrl:
     process.env.NEXT_PUBLIC_API_BASE_URL ||
     // Default for local dev
-    "http://localhost:5000",
-  appUrl: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+    "http://localhost:4000",
+  appUrl: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3100",
 };
